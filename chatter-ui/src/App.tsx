4c973ab9943db43b82b7e029a4dbaf029c6af73d
iantab <11763662+iantab@users.noreply.github.com>
2025-04-16 13:09:50 -0400

    Added initial backend code with graphql and mongodb configuration

--- chatter-ui/src/App.tsx
+++ chatter-ui/src/App.tsx
@@ -1,27 +1,27 @@
 import {
-    Container,
-    createTheme,
-    CssBaseline,
-    ThemeProvider
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
 } from "@mui/material";
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import Router from "./components/routes";
 
 const darkTheme = createTheme({
-    palette: {
-        mode: 'dark'
-    }
-})
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <CssBaseline/>
-            <Container>
-                <RouterProvider router={Router}/>
-            </Container>
-        </ThemeProvider>
-    )
-}
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <RouterProvider router={Router} />
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- chatter-ui/src/components/auth/login.tsx
+++ chatter-ui/src/components/auth/login.tsx
@@ -1,18 +1,17 @@
-import {Link} from "react-router-dom";
-import {Link as MUILink} from "@mui/material"
+import { Link } from "react-router-dom";
+import { Link as MUILink } from "@mui/material";
 import Auth from "./auth";
 
 const Login = () => {
-    return (<>
-        <Auth submitLabel="Login" onSubmit={async () => {
-        }}>
-            <Link to={'/signup'} style={{alignSelf: 'center'}}>
-                <MUILink>
-                    Signup
-                </MUILink>
-            </Link>
-        </Auth>
-    </>);
-}
+  return (
+    <>
+      <Auth submitLabel="Login" onSubmit={async () => {}}>
+        <Link to={"/signup"} style={{ alignSelf: "center" }}>
+          <MUILink>Signup</MUILink>
+        </Link>
+      </Auth>
+    </>
+  );
+};
 
 export default Login;
