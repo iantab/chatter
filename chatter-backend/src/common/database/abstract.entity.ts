77c196d23b2515722f54ad0adc7080d5594d3931
iantab <11763662+iantab@users.noreply.github.com>
2025-04-16 14:38:44 -0400

    Updated user entity to extend abstract entity

--- chatter-backend/src/common/database/abstract.entity.ts
+++ chatter-backend/src/common/database/abstract.entity.ts
@@ -1,8 +1,11 @@
 import { Prop, Schema } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @Schema()
-export class AbstractDocument {
+@ObjectType({ isAbstract: true })
+export class AbstractEntity {
   @Prop({ type: SchemaTypes.ObjectId })
+  @Field(() => ID)
   _id: Types.ObjectId;
 }
