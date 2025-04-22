c02c0f76884e194b9c0b04ac075b05ba1a206307
iantab <11763662+iantab@users.noreply.github.com>
2025-04-22 18:07:15 -0400

    Updated header links based on whether the user is logged in or not

--- chatter-ui/src/components/auth/guard.tsx
+++ chatter-ui/src/components/auth/guard.tsx
@@ -1,6 +1,7 @@
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import { useGetMe } from "../../hooks/useGetMe";
 import excludedRoutes from "../../constants/excluded-routes";
+import { authenticatedVar } from "../../constants/authenticated";
 
 interface GuardProps {
   children: JSX.Element;
@@ -9,6 +10,11 @@ interface GuardProps {
 const Guard = ({ children }: GuardProps) => {
   const { data: user } = useGetMe();
 
+  useEffect(() => {
+    if (user) {
+      authenticatedVar(true);
+    }
+  }, [user]);
   return (
     <>
       {excludedRoutes.includes(window.location.pathname)
--- chatter-ui/src/components/header/navigation.tsx
+++ chatter-ui/src/components/header/navigation.tsx
@@ -1,16 +1,22 @@
 import Box from "@mui/material/Box";
 import { Button } from "@mui/material";
+import { Page } from "../../interfaces/page.interface";
+import router from "../../components/routes";
 
 interface NavigationProps {
-  pages: string[];
+  pages: Page[];
 }
 
 const Navigation = ({ pages }: NavigationProps) => {
   return (
     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
       {pages.map((page) => (
-        <Button key={page} sx={{ my: 2, color: "white", display: "block" }}>
-          {page}
+        <Button
+          key={page.title}
+          onClick={() => router.navigate(page.path)}
+          sx={{ my: 2, color: "white", display: "block" }}
+        >
+          {page.title}
         </Button>
       ))}
     </Box>
